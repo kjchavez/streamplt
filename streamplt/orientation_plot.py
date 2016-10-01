3b9e051d7501c6cab83f6c43a89572bc0d58b621
Kevin Chavez <kjchavez@stanford.edu>
2016-10-01 12:52:15 -0700

    orientation plot with support for quaternions

--- streamplt/orientation_plot.py
+++ streamplt/orientation_plot.py
@@ -10,10 +10,62 @@ def scale_to_len(q1, q2, q3, length=1):
     c  = float(length) / np.linalg.norm([q1, q2, q3])
     return (q1*c, q2*c, q3*c)
 
+def quat_to_euler(q):
+    """ Converts quaternion 'q' to Euler angles. """
+    q = np.array(q) / np.linalg.norm(q)
+    phi = np.arctan2(2*(q[0]*q[1] + q[2]*q[3]), 1 - 2*(q[1]**2 + q[2]**2))
+    theta = np.arcsin(2*(q[0]*q[2] - q[3]*q[1]))
+    psi = np.arctan2(2*(q[0]*q[3] + q[1]*q[2]), 1-2*(q[2]**2+q[3]**2))
+    return (phi, theta, psi)
+
+def quat_conjugate(q):
+    return np.array([q[0], -q[1], -q[2], -q[3]])
+
+def quat_prod(a, b):
+    c = np.empty((4,))
+    c[0] = a[0]*b[0] - np.dot(a[1:], b[1:])
+    c[1:] = a[0]*b[1:]+b[0]*a[1:]+np.cross(a[1:], b[1:])
+    return c
+
+def rotate_by_quat(p, q):
+    if not isinstance(q, np.ndarray):
+        q = np.array(q)
+
+    # Normalize to unit length
+    q = q / np.linalg.norm(q)
+
+    # Augment 3-vector.
+    p = np.array([0, p[0], p[1], p[2]])
+
+    # Perform rotation by the quaternion.
+    p_r = quat_prod(q, quat_prod(p, quat_conjugate(q)))
+    return p_r[1:]
+
 class Orientation3dPlot(object):
+    """ A 3D plot of streaming orientation data.
+
+    Supports various input modes:
+    ===========================================================================
+    * 'xyz': input data is a 3-vector, possibly unnormalized, giving a
+             direction from the origin.
+    * 'wxyz': input data is a quaternion giving an orientation with respect to
+              the positive x-axis.
+    """
     MAX_NUM_FRAMES = 100000000
-    def __init__(self, address, interval_ms=20, lw=2, arrow_len=1.0, fig=None, ax=None):
-        self.listener = DataListener(address, buffer_width=3+1,
+    def __init__(self, address, input_mode='xyz',
+                 interval_ms=20, lw=2, arrow_len=1.0,
+                 fig=None, ax=None):
+        assert (input_mode in ('xyz', 'wxyz')), \
+                "Unsupported mode: " + input_mode
+        self.input_mode = input_mode
+
+        # Setup transformation function per input mode.
+        self.transformation_fn = lambda x: x
+        if input_mode == 'wxyz':
+            self.transformation_fn =  lambda q : rotate_by_quat([1,0,0], q)
+
+        buffer_width = 1 + (3 if input_mode == 'xyz' else 4)
+        self.listener = DataListener(address, buffer_width=buffer_width,
                 buffer_length=10)
         self.listener.start()
 
@@ -56,7 +108,8 @@ class Orientation3dPlot(object):
 
         self.ax.collections.remove(self.quiver)
 
-        t, q1, q2, q3 = data[-1]
+        t = data[-1][0]
+        q1, q2, q3 = self.transformation_fn(data[-1][1:])
         q1, q2, q3 = scale_to_len(q1, q2, q3)
         self.quiver = self.ax.quiver([0], [0], [0],
                         [q1], [q2], [q3],
@@ -69,10 +122,11 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--address", '-a', default= "ipc:///tmp/stream",
                         help="Address to listen to for incoming data.")
+    parser.add_argument("--mode", '-m', choices=["xyz", "wxyz"], default="xyz")
 
     args = parser.parse_args()
 
-    s = Orientation3dPlot(args.address)
+    s = Orientation3dPlot(args.address, input_mode=args.mode)
     plt.show()
 
 if __name__ == "__main__":
