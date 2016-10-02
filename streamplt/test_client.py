c8860376368fafb987f162de077bec42aa3d66b9
Kevin Chavez <kjchavez@stanford.edu>
2016-10-01 22:52:55 -0700

    Cleanup test client and commited files.

--- streamplt/test_client.py
+++ streamplt/test_client.py
@@ -1,19 +1,36 @@
 import zmq
 import random
 import time
+import argparse
+import numpy as np
 
 context = zmq.Context()
+class TestClient:
+    def __init__(self, dim, delay=0.5, address="ipc:///tmp/stream"):
+        self.dim = dim
+        self.delay = delay
+        self.format_string = ",".join("%f" for _ in xrange(dim+1))
+        self.sock = context.socket(zmq.PUB)
+        self.sock.bind(address)
 
-sock = context.socket(zmq.PUB)
-sock.bind("ipc:///tmp/stream")
+    def run(self):
+        x = np.random.rand(self.dim) - 0.5
+        while True:
+            x += np.random.rand(self.dim) - 0.5
+            t = time.time()
+            self.sock.send(self.format_string % ((t,)+tuple(x)))
+            time.sleep(self.delay)
 
-x = 0.0
-y = 0.0
-z = 0.0
-while True:
-    t = time.time()
-    x += 2*(random.random() - 0.5)
-    y += 1*(random.random() - 0.5)
-    z += 1*(random.random() - 0.5)
-    sock.send("%f, %f, %f, %f" % (t, x, y, z))
-    time.sleep(0.01)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dim", "-d", type=int, default=3)
+    parser.add_argument("--address", "-a", default="ipc:///tmp/stream")
+    parser.add_argument("--time", "-t", type=float, default=0.5,
+                        help="Time to wait between subsequent samples.")
+    args = parser.parse_args()
+
+    client = TestClient(args.dim, delay=args.time, address=args.address)
+    client.run()
+
+if __name__ == "__main__":
+    main()
